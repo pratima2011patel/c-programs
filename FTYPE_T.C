e99b1c677a1649e2e73bda67f278b066575933fd
pratima2011patel <78414900+pratima2011patel@users.noreply.github.com>
2021-02-05 02:28:56 -0800

    Add files via upload

--- FTYPE_T.C
+++ FTYPE_T.C
@@ -6,14 +6,12 @@ int at(int , int);
 /*int fun(int, int); */
 void main()
 {
-int a=2,as,l=3,b=4,atr;
+int as,l=3,b=4,atr;
 clrscr();
-/*cout<<a<<b<<endl; */
-/*as=asq(a);
-atr=at(l,b); */
 at(l,b);
 getch();
 }
+
 int at(int x, int y)
 {
 int t=(x*y);
