3d7520669edcb23554b1c0bf32c7dfbd7b2901a9
pratima2011patel <78414900+pratima2011patel@users.noreply.github.com>
2021-02-08 14:30:53 +0530

    Add files via upload

---
+++ PC4.C
@@ -0,0 +1,14 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+clrscr();
+ar();
+getch();
+}
+int ar()
+{
+float r=2,p=3.14,area;
+ area=(2*p*r*r);
+printf("The Perimeter of Circel is :-%f",area);
+}
