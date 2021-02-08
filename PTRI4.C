1af72c79bb3e059eefac043f4b90405205cf15a3
pratima2011patel <78414900+pratima2011patel@users.noreply.github.com>
2021-02-08 14:49:18 +0530

    Add files via upload
    Example of Function with No argument and return value

---
+++ PTRI4.C
@@ -0,0 +1,16 @@
+
+
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+clrscr();
+as();
+getch();
+}
+int as()
+{
+int a=4, b=2,c=3,area;
+area=a+b+c;
+printf("The Perimeter of Triangle:-%d",area);
+}
