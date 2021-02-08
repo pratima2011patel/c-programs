8998cef8cbf9d2a71229a0de9ce11a4808f73867
pratima2011patel <78414900+pratima2011patel@users.noreply.github.com>
2021-02-08 14:28:15 +0530

    Add files via upload
    Example of Function with No argument and return value

---
+++ AC4.C
@@ -0,0 +1,15 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+clrscr();
+ar();
+getch();
+}
+int ar()
+{
+float r=2,h=4,p=3.14;
+int area=(p*r*r*h);
+printf("The area of Cone is :-%d",area);
+;
+}
