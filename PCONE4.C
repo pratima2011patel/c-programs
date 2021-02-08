156aa6ce4ad052b402926169600d0a41db6f1716
pratima2011patel <78414900+pratima2011patel@users.noreply.github.com>
2021-02-08 14:38:18 +0530

    Add files via upload

---
+++ PCONE4.C
@@ -0,0 +1,14 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+clrscr();
+as();
+getch();
+}
+int as()
+{
+int a=2,b=3,area;
+area=((a*a)+(b*b));
+printf("The Perimeter of Cone is :-%d",area);
+}
