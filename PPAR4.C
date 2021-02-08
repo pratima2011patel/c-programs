267ae6d277a306d358d1e21a4155c25edffd5e26
pratima2011patel <78414900+pratima2011patel@users.noreply.github.com>
2021-02-08 14:39:43 +0530

    Add files via upload
    Example of Function with No argument and return value

---
+++ PPAR4.C
@@ -0,0 +1,14 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+clrscr();
+as();
+getch();
+}
+int as()
+{
+int a=2,b=3,area;
+area=((2*a)+(2*b));
+printf("The Perimeter of Parallelogram is :-%d",area);
+}
