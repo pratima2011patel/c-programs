5d664516cca4fa88b34fead633ed6000206cb0ba
pratima2011patel <78414900+pratima2011patel@users.noreply.github.com>
2021-02-08 14:36:39 +0530

    Add files via upload
    Example of Function with No argument and return value

---
+++ PS4.C
@@ -0,0 +1,14 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+clrscr();
+as();
+getch();
+}
+int as()
+{
+int l=2,area;
+area=(4*l*l*l*l);
+printf("The Perimeter of Square is :-%d",area);
+}
