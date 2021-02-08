b5ad5a99b5f04900fd0e3971cc9db87b68860d3d
pratima2011patel <78414900+pratima2011patel@users.noreply.github.com>
2021-02-08 14:46:01 +0530

    Add files via upload
    Example of Function with No argument and return value

---
+++ ATRAP4.C
@@ -0,0 +1,14 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+clrscr();
+as();
+getch();
+}
+int as()
+{
+int a=4, b=2,h=3,area;
+area=(0.5*((a+b)*h));
+printf("The Area of Trapezium:-%d",area);
+}
