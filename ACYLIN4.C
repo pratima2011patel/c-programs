a204ba3f429e8a862c94f2cd9af0de90bb6f70cc
pratima2011patel <78414900+pratima2011patel@users.noreply.github.com>
2021-02-08 14:33:36 +0530

    Add files via upload
    Example of Function with No argument and return value

---
+++ ACYLIN4.C
@@ -0,0 +1,14 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+clrscr();
+ar();
+getch();
+}
+int ar()
+{
+float r=2,h=3,p=3.14,area;
+area=(h*p*r*r);
+printf("The Perimeter of Circelis :-%f",area);
+}
