2725bf7289c51cd641ba8ddc607380ef75910a1d
pratima2011patel <78414900+pratima2011patel@users.noreply.github.com>
2021-02-12 14:21:00 +0530

    Add files via upload
    55555
    44444
    33333
    22222
    11111

---
+++ PAT8.C
@@ -0,0 +1,16 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+int i,j,n=5;
+clrscr();
+for(i=1;i<=5;i++)
+{	for(j=1;j<=5;j++)
+	{
+	printf("%d",n);
+	}
+	n--;
+	printf("\n");
+}
+getch();
+}
