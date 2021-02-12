92df0102663af683d28296316c2eb6b819aaf10b
pratima2011patel <78414900+pratima2011patel@users.noreply.github.com>
2021-02-12 14:57:38 +0530

    Add files via upload
    1
    12
    123
    1234
    12345

---
+++ PAT13.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,n=1;
+clrscr();
+for(i=0;i<=4;i++)
+{
+	for(j=1;j<=i;j++)
+	{
+	printf("%d",j);
+	}
+	printf("\n");
+}
+
+getch();
+}
+
