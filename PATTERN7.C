b3a68bfaff16aa5ae25eee6f4f3205653d92bf2e
pratima2011patel <78414900+pratima2011patel@users.noreply.github.com>
2021-02-17 14:49:19 +0530

    Add files via upload
                         *
                       *   *
                    *    *    *
                *     *     *    *

---
+++ PATTERN7.C
@@ -0,0 +1,21 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+
+int i=4,j=7,k,n=3;
+clrscr();
+for(i=1;i<=4;i++)
+{
+	for(k=3;k>=i;k--)
+	{
+	printf(" ");
+	}
+	for(j=1;j<=i;j++)
+	{
+	printf("* ");
+	}
+printf("\n");
+}
+getch();
+}
