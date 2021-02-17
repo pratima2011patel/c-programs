dfd7e6ea90d4facec4bd2e2bc417c451e40a3ff5
pratima2011patel <78414900+pratima2011patel@users.noreply.github.com>
2021-02-17 17:06:34 +0530

    Add files via upload
           1
        2 2
     3 3 3

---
+++ R_PAT11.C
@@ -0,0 +1,21 @@
+	#include<conio.h>
+#include<stdio.h>
+void main()
+{
+int i,j,k,n=1;
+clrscr();
+for(i=1;i<=3;i++)
+{
+	for(k=2;k>=i;k--)
+	{
+	printf(" ");
+	}
+	for(j=1;j<=i;j++)
+	{
+	printf("%d",n);
+	}n++;
+	printf("\n");
+    }
+
+getch();
+}
